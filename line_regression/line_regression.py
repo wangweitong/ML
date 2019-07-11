e149300cf2b859503b17c436bff6cfc5fb2d352f
wangweitong <1097828409@qq.com>
2019-07-11 21:08:02 +0800

    完成大部分，缺少PCA和SVD

--- line_regression/line_regression.py
+++ line_regression/line_regression.py
@@ -1,3 +1,6 @@
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+
 data = pd.read_csv("E:\data\input\champs-scalar-coupling\\train.csv")
 data['scalar_coupling_constant'] = abs(data['scalar_coupling_constant'])
 test = data.sample(frac=0.02)[['atom_index_0', 'atom_index_1', 'scalar_coupling_constant']]
@@ -6,7 +9,7 @@ features = test[['atom_index_0', 'atom_index_1']]
 label = abs(test['scalar_coupling_constant'])
 
 # C 是正则化系数，越小，正则化越强
-lr = linear_model.LinearRegression().fit(features, label)
+lr = LinearRegression().fit(features, label)
 
 print(lr.score(features, label))
 Z = lr.predict(features * 2)
